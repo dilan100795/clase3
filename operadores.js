ae6c2d26e60c4f63c61bd44c7fd5b691eafda606
dilan100795 <zzdilan123@gmail.com>
2022-05-30 09:50:26 -0300

    nuevorepo

---
+++ operadores.js
@@ -0,0 +1,71 @@
+console.clear()
+/* De Asignacion */
+
+let asignacion = 143
+
+/* Aritmeticos */
+
+let num1 = 55 
+let num2 = 45
+let num3 = 912
+let num4 = 2611
+
+let suma = num1 + num2 // Suma se hace con +
+let resta = num1 - num2 // Resta se hace con -
+let multiplicacion = num1 * num2 // Multiplicacion se hace con *
+let division = num1 / num2 // division se hace con /
+let modulo = num4 % 2
+/*console.log(modulo)*/
+/*console.log(resta);*/
+/* Incremento & Decremento */
+let num5 = 55 
+let num6 = 45
+let num7 = 912
+let num8 = 2611
+
+let incremento = ++num6
+let decremento = --num7
+console.log(incremento);
+/* Asignacion con suma */ /*para declarar una suma o resta/*
+/* console.log(suma += resta); */
+/* suma = suma + suma */
+/* Asignacion con resta */
+/* console.log(suma -= resta); */
+/* suma = suma - resta */
+
+let num9 = 55 
+let num10 = "55"
+/* Comparacion Simple */
+
+let igualdadSimple = num10 == num10
+let desigualdadSimple = num9 != num10
+
+console.log(igualdadSimple);
+/* Comparacion Estricta */
+
+let igualdadEstricta = num9 === num10
+let desigualdadEstricta = num9 !== num10
+
+/* console.log(igualdadEstricta); */
+
+/* Negacion */
+/* console.log(!true); */ //Esto devuelve False
+
+/* Operadores Relacionales */
+let mayorQue = num1 > num9
+let menorQue = num1 < num2
+let mayorIgualQue = num1 >= num10
+let menorIgualQue = num1 <= num2
+
+/* console.log(mayorIgualQue) */
+
+/* Operadores Logicos */
+
+/* And */
+let condicion = num1 === num9 && num1 === num10
+/* condicion = true && false  esto es igual a false*/
+
+/* Or */
+let condicion2 = num1 === num2 || num1 === num10
+/* condicion2 = false && false  esto es igual a false */
+console.log(condicion2);
---
+++ variables.js
@@ -0,0 +1,23 @@
+let pepito = 'Hola gente de la comision 17'
+var variable2 = 'Estamos arrancando el curso de programacion'
+const variable3 = 'Comision 17'
+
+console.log(variable3);
+console.log(pepito);
+//* tipos de datos*//
+let string = "Esto es un string" //Siempre esta declarado entre comillas
+
+let number = 65;
+let decimal = 54.3;
+let texto = "pekas";
+let booleano = true; // Solo es true o false
+let nulo = null;
+let sinDefinir = undefined
+let nan = "true"*45;
+
+console.log(nulo); 
+console.log(decimal);
+console.log(booleano);
+console.log(nan);
+console.log(number);
+console.log(texto);
